86f72515ad86681c27999268f814dd6bcefb8a92
Herve Ah-Leung <h.ah-leung@Hervito-ResilientPlc.local>
2019-02-27 17:24:27 +0000

    Add logs

--- test_utils/dynamo-local.go
+++ test_utils/dynamo-local.go
@@ -3,14 +3,16 @@ package test_utils
 import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
+	log "github.com/sirupsen/logrus"
 	"github.com/guregu/dynamo"
 	"github.com/rs/xid"
 )
 
 // Creates a new, randomly named, Dynamo table using the interface provided
 func CreateTable(table interface{}) (string, error) {
+	log.Info("Trying to create a table")
 	cfg := aws.Config{
-		Endpoint:                      aws.String("http://127.0.0.1::8000"),
+		Endpoint:                      aws.String("http://127.0.0.1:8000"),
 		Region:                        aws.String("eu-west-2"),
 		CredentialsChainVerboseErrors: aws.Bool(true),
 	}
