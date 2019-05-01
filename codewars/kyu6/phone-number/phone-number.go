f85399bdbb5a09f2ba09ce4d5e496dbace369f1f
Hervé Ah-Leung <herve.ahleung@gmail.com>
2019-05-01 18:45:37 +0100

    Codewars - Kyu6 > Create phone number - done

--- codewars/kyu6/phone-number/phone-number.go
+++ codewars/kyu6/phone-number/phone-number.go
@@ -1,18 +1,25 @@
 package phonenumber
 
 import (
-	"log"
+	"fmt"
 	"strconv"
 	"strings"
 )
 
 func CreatePhoneNumber(numbers [10]uint) string {
 	numbersString := make([]string, 10)
-	for i, _ := range numbersString {
+	for i := range numbersString {
 		numbersString[i] = string(strconv.Itoa(int(numbers[i])))
 	}
 
-	log.Println(numbersString)
-	log.Println(strings.Join(numbersString[0:3], ""))
-	return ""
+	return fmt.Sprintf("(%s) %s-%s", join(numbersString[0:3]), join(numbersString[3:6]), join(numbersString[6:10]))
 }
+
+func join(portion []string) string {
+	return strings.Join(portion, "")
+}
+
+// Lol
+// func CreatePhoneNumber(n [10]uint) string {
+//   return fmt.Sprintf("(%d%d%d) %d%d%d-%d%d%d%d", n[0], n[1], n[2], n[3], n[4], n[5], n[6], n[7], n[8], n[9])
+// }
