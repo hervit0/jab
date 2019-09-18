79c446bbdcc1cbd8802ac852d1a436d563ed3e19
Hervito <herve.ahleung@gmail.com>
2019-09-19 00:09:49 +0100

    Daily - 2019-09-18 > Second iteration with Map

--- daily/20190918.go
+++ daily/20190918.go
@@ -4,7 +4,37 @@ package daily
 //For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 //Bonus: Can you do this in one pass?
 
+// Slightly faster, a 2N-ish algo as it needs to lookup into a map
 func Sum(numbers []int, sum int) bool {
+	dict := map[int]map[int]bool{}
+
+	for i, number := range numbers {
+		reverseNum := sum - number
+		indexesDict, ok := dict[reverseNum]
+		if !ok {
+			indexesDict = map[int]bool{}
+		}
+		indexesDict[i] = true
+		dict[reverseNum] = indexesDict
+	}
+
+	var match bool
+	for i, number := range numbers {
+		indexesDict, ok := dict[number]
+		if ok {
+			delete(indexesDict, i)
+			if len(indexesDict) != 0 {
+				match = true
+				break
+			}
+		}
+	}
+
+	return match
+}
+
+// This is an N^2 algo
+func SumSlow(numbers []int, sum int) bool {
 	reverseNumbers := make([]int, len(numbers))
 
 	for i, number := range numbers {
